1ae5a63975948fef8ec7f3b2617433c733fbba9b
Jingo88 <jcory.ng@gmail.com>
2015-02-07 16:36:11 -0500

    got bcrypt working in sessions

--- sessions/app/controllers/secret_controller.rb
+++ sessions/app/controllers/secret_controller.rb
@@ -1,12 +1,22 @@
 
 class SecretController < ApplicationController
-	
+
 	def show
-		valid_user = session[:valid_user]
-		if valid_user
+		@user = User.find_by(id: session[:user_id])
+
+		if @user
 			render :show
 		else
 			redirect_to '/login'
 		end
 	end
+
+	# 	# This is for sessions
+	# 	valid_user = session[:valid_user]
+	# 	if valid_user
+	# 		render :show
+	# 	else
+	# 		redirect_to '/login'
+	# 	end
+	# end
 end
--- sessions/app/controllers/session_controller.rb
+++ sessions/app/controllers/session_controller.rb
@@ -4,15 +4,26 @@ class SessionController < ApplicationController
 	end
 
 	def create
-		# will look into config / secrets / secret_password
-		if params[:password] == Rails.application.secrets.secret_password
-			session[:valid_user] = true
+		# this is used for bcrypt. More secure than sessions
+		user = User.find_by(username: params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
 			redirect_to '/secret'
 		else
-		
+			@error = true
 			render :new
-	    end
+		end
 	end
+	# 	# will look into config / secrets / secret_password
+	# 	if params[:password] == Rails.application.secrets.secret_password
+	# 		session[:valid_user] = true
+	# 		redirect_to '/secret'
+	# 	else
+		
+	# 		render :new
+	#     end
+	# end
 
 	def destroy
 		# reset_session will kill the existing session
